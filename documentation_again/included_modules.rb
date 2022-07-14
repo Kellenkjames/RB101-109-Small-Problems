33a2394b82a7462a91f5b6daae96a1c3eb3fa5f8
Kellen James <Kellenkjames@gmail.com>
2022-07-14 13:37:16 -0400

    checkpoint, updates notes in the file

---
+++ documentation_again/included_modules.rb
@@ -0,0 +1,11 @@
+#* Included Modules 
+
+# Use irb to run the following code:
+
+# Find the documentation for the #min method and change the above code to print the two smallest values in the Array.
+
+# https://docs.ruby-lang.org/en/2.7.0/Array.html#method-i-min
+
+a = [5, 9, 3, 11]
+puts a.min(2)
+
