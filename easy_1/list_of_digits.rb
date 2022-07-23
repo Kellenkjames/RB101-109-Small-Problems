52cf7988e60897750d17cc633f98a4850c782b49
Kellen James <Kellenkjames@gmail.com>
2022-07-23 12:36:14 -0400

    updates indentation for example / test cases

--- easy_1/list_of_digits.rb
+++ easy_1/list_of_digits.rb
@@ -25,10 +25,10 @@
 
 * E - Examples / Test Cases 
 
-p digit_list(12345) == [1, 2, 3, 4, 5]     # => true
-p digit_list(7) == [7]                     # => true
-p digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
-p digit_list(444) == [4, 4, 4]             # => true
+p digit_list(12345) == [1, 2, 3, 4, 5]                   # => true
+p digit_list(7) == [7]                                            # => true
+p digit_list(375290) == [3, 7, 5, 2, 9, 0]            # => true
+p digit_list(444) == [4, 4, 4]                              # => true
 
 ------------------------------------------------------------------------------
 
@@ -73,8 +73,8 @@ def digit_list(number)
   digits
 end 
 
-puts digit_list(12345) == [1, 2, 3, 4, 5]     # => true
-puts digit_list(7) == [7]                     # => true
-puts digit_list(375290) == [3, 7, 5, 2, 9, 0] # => true
-puts digit_list(444) == [4, 4, 4]             # => true
+p digit_list(12345) == [1, 2, 3, 4, 5]                   # => true
+p digit_list(7) == [7]                                            # => true
+p digit_list(375290) == [3, 7, 5, 2, 9, 0]            # => true
+p digit_list(444) == [4, 4, 4]                              # => true
 
